d2ea07b4b5f407721b34703eeb69587213aeabbe
Peter Bean <binh@sunrise.vn>
2021-05-08 13:49:54 +0700

    add apigateway restapi datasource

--- lambda/terraform/query-test2.tf
+++ lambda/terraform/query-test2.tf
@@ -22,7 +22,7 @@ resource "aws_api_gateway_method" "exampleservice-test2-query-api-proxy" {
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = data.aws_lambda_function.exampleservice-test2-query-api.invoke_arn
+  uri                     = aws_lambda_function.exampleservice-test2-query-api.invoke_arn
 }
   
 
