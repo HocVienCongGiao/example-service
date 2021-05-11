4b5a43dcc3d3cf77f736f83f023ebdb9658d71c1
binhle410 <binh@sunrise.vn>
2021-05-11 20:07:45 +0700

    Fix lint and codestyle

--- lambda/cognito/src/main.rs
+++ lambda/cognito/src/main.rs
@@ -3,10 +3,12 @@ extern crate rusoto_dynamodb;
 
 use std::default::Default;
 
-use rusoto_core::{Region, Client};
+use rusoto_cognito_idp::{
+    CognitoIdentityProvider, CognitoIdentityProviderClient, ListUsersRequest,
+};
+use rusoto_core::{Client, Region};
 use rusoto_dynamodb::{DynamoDb, DynamoDbClient, ListTablesInput};
-use rusoto_s3::{S3, S3Client};
-use rusoto_cognito_idp::{CognitoIdentityProviderClient, CognitoIdentityProvider, ListUsersRequest};
+use rusoto_s3::{S3Client, S3};
 // use rusoto_credential::{ProfileProvider};
 // https://docs.rs/rusoto_cognito_idp/0.46.0/rusoto_cognito_idp/
 use lambda_http::{handler, lambda, Context, IntoResponse, Request};
@@ -28,7 +30,8 @@ async fn cognito(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
     // let aws_client = Client::new_with(default_provider, dispatcher);
     let aws_client = Client::shared();
 
-    let rusoto_cognito_idp_client = CognitoIdentityProviderClient::new_with_client(aws_client.clone(), Region::ApSoutheast1);
+    let rusoto_cognito_idp_client =
+        CognitoIdentityProviderClient::new_with_client(aws_client.clone(), Region::ApSoutheast1);
     // let create_user_request = AdminCreateUserRequest {
     //     desired_delivery_mediums: None,
     //     force_alias_creation: None,
@@ -44,36 +47,37 @@ async fn cognito(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
         filter: None,
         limit: None,
         pagination_token: None,
-        user_pool_id: "ap-southeast-1_vmFHg7JIC".to_string()
+        user_pool_id: "ap-southeast-1_vmFHg7JIC".to_string(),
     };
-    match rusoto_cognito_idp_client.list_users(list_user_request).sync() {
-        Ok(response) => {
-            match response.users {
-                Some(user_types) => {
-                    println!("User Type here");
-                    for user_type in user_types {
-                        println!("{}", user_type.username.unwrap())
-                    }
+    match rusoto_cognito_idp_client
+        .list_users(list_user_request)
+        .sync()
+    {
+        Ok(response) => match response.users {
+            Some(user_types) => {
+                println!("User Type here");
+                for user_type in user_types {
+                    println!("{}", user_type.username.unwrap())
                 }
-                _ => {}
             }
+            None => println!("No buckets in region!"),
+        },
+        Err(error) => {
+            println!("Error: {:?}", error);
         }
-        _ => {}
     }
 
-    let s3client = S3Client::new_with_client(aws_client.clone(), Region::ApSoutheast1);
+    let s3client = S3Client::new_with_client(aws_client, Region::ApSoutheast1);
     match s3client.list_buckets().sync() {
-        Ok(output) => {
-            match output.buckets {
-                Some(bucket_list) => {
-                    println!("Bucket to be printed here");
-                    for bucket in bucket_list {
-                        println!("{}", bucket.name.unwrap());
-                    }
+        Ok(output) => match output.buckets {
+            Some(bucket_list) => {
+                println!("Bucket to be printed here");
+                for bucket in bucket_list {
+                    println!("{}", bucket.name.unwrap());
                 }
-                None => println!("No buckets in region!"),
             }
-        }
+            None => println!("No buckets in region!"),
+        },
         Err(error) => {
             println!("Error: {:?}", error);
         }
@@ -83,27 +87,25 @@ async fn cognito(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
     let list_tables_input: ListTablesInput = Default::default();
 
     match client.list_tables(list_tables_input).sync() {
-        Ok(output) => {
-            match output.table_names {
-                Some(table_name_list) => {
-                    println!("Tables in database:");
+        Ok(output) => match output.table_names {
+            Some(table_name_list) => {
+                println!("Tables in database:");
 
-                    for table_name in table_name_list {
-                        println!("{}", table_name);
-                    }
+                for table_name in table_name_list {
+                    println!("{}", table_name);
                 }
-                None => println!("No tables in database!"),
             }
-        }
+            None => println!("No tables in database!"),
+        },
         Err(error) => {
             println!("Error: {:?}", error);
         }
     }
 
-// creating an application/json response
+    // creating an application/json response
     Ok(json!({
-"message": "Hey cognito!"
-}))
+    "message": "Hey cognito!"
+    }))
 }
 
 #[cfg(test)]
@@ -116,7 +118,7 @@ mod tests {
         let expected = json!({
         "message": "Hey cognito!"
         })
-            .into_response();
+        .into_response();
         let response = cognito(request, Context::default())
             .await
             .expect("expected Ok(_) value")
