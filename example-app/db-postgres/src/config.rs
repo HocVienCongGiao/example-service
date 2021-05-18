72c7f6d9bc88b5bd0a47af326bf1f4da605e90ea
binhle410 <binh@sunrise.vn>
2021-05-18 15:00:00 +0700

    Add config for different OS for embedded Postgres

---
+++ example-app/db-postgres/src/config.rs
@@ -0,0 +1,40 @@
+pub struct Config {
+    pub(crate) db_user: Option<String>,
+    pub(crate) db_password: Option<Vec<u8>>,
+    pub(crate) db_name: Option<String>,
+    pub(crate) db_options: Option<String>,
+    pub(crate) application_name: Option<String>,
+    pub(crate) os_type: String,
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config::new()
+    }
+}
+
+impl Config {
+    pub fn new() -> Config {
+        let ostype_env_result = std::env::var("OSTYPE");
+        let mut ostype: String = "windows".to_string();
+        let ostype_env_lowercase = ostype_env_result.unwrap_or_default().to_lowercase();
+        match &ostype_env_lowercase[0..5] {
+            "linux" => {
+                ostype = String::from("linux");
+            }
+            "darwi" => {
+                ostype = String::from("darwin");
+            }
+            _ => {}
+        }
+
+        Config {
+            db_user: None,
+            db_password: None,
+            db_name: None,
+            db_options: None,
+            application_name: None,
+            os_type: ostype,
+        }
+    }
+}
--- example-app/db-postgres/src/lib.rs
+++ example-app/db-postgres/src/lib.rs
@@ -1,5 +1,8 @@
 use domain::test_func;
+use tokio_postgres::types::ToSql;
 use tokio_postgres::{Error, NoTls};
+
+pub mod config;
 
 mod embedded {
     use refinery::{embed_migrations, Error};
@@ -11,7 +14,7 @@ async fn main() -> Result<(), Error> {
     println!("Start of main()");
     // connect to the DB
     let (mut client, connection) = tokio_postgres::connect(
-        "postgresql://postgres:password@localhost:6543/database_name",
+        "user=postgres password=password host=localhost port=6543 dbname=database_name",
         //         tokio_postgres::connect("postgresql://postgres:password@localhost/test", NoTls).await?;
         NoTls,
     )
@@ -46,6 +49,34 @@ async fn main() -> Result<(), Error> {
         )
         .await;
 
+    let query = client.query_one(
+        "
+            SELECT * FROM author_initial      
+            WHERE id = $1        
+    ",
+        &[&2],
+    );
+
+    let result = query.await;
+
+    // use std::io::{stdin, stdout, Write};
+    // let mut s = String::new();
+    // print!("Please enter some text: ");
+    // let _ = stdout().flush();
+    // stdin()
+    //     .read_line(&mut s)
+    //     .expect("Did not enter a correct string");
+    // if let Some('\n') = s.chars().next_back() {
+    //     s.pop();
+    // }
+    // if let Some('\r') = s.chars().next_back() {
+    //     s.pop();
+    // }
+    // println!("You typed: {}", s);
+
+    let row = result.unwrap();
+    let author_name = row.get::<&str, &str>("name");
+    println!("got row {}", author_name);
     client.batch_execute(
         "
         CREATE TABLE IF NOT EXISTS book  (
@@ -86,10 +117,22 @@ mod tests {
             migration_dir: None,
         };
 
+        let config = crate::config::Config::new();
+        let postgres_os: OperationSystem;
+        match config.os_type.as_str() {
+            "linux" => postgres_os = OperationSystem::Linux,
+            "darwin" => postgres_os = OperationSystem::Darwin,
+            _ => postgres_os = OperationSystem::Windows,
+        }
+
         /// Postgresql binaries download settings
         let fetch_settings = FetchSettings {
             host: "https://repo1.maven.org".to_string(),
-            operating_system: OperationSystem::Windows,
+            operating_system: if config.os_type == "windows" {
+                OperationSystem::Windows
+            } else {
+                OperationSystem::Linux
+            },
             architecture: Architecture::Amd64,
             version: PG_V13,
         };
@@ -142,6 +185,8 @@ mod tests {
 
     #[tokio::test]
     async fn it_works() {
+        dotenv::dotenv().ok();
+        println!("testing env {}", std::env::var("HELLO").unwrap());
         println!("Calling main()");
         let mut pg = init_embedded_postgres().await;
         main().await.expect("Failing for no reason");
