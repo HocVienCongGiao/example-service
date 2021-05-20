7c31b8a30a1bbf82fc61f4212a9819cca7c886b4
binhle410 <binh@sunrise.vn>
2021-05-20 11:45:55 +0700

    Refactor to query.rs

--- example-app/db-postgres/src/test1_gateway/mod.rs
+++ example-app/db-postgres/src/test1_gateway/mod.rs
@@ -1,5 +1,4 @@
 use async_trait::async_trait;
-use tokio_postgres::types::ToSql;
 use tokio_postgres::Client;
 
 mod mutation;
@@ -12,15 +11,7 @@ pub struct Test1SimpleRepository {
 #[async_trait]
 impl domain::boundaries::Test1DbGateway for Test1SimpleRepository {
     async fn exists_by_name(&self, name: String) -> bool {
-        let stmt = (*self)
-            .client
-            .prepare("SELECT * FROM author_initial WHERE name = $1")
-            .await
-            .unwrap();
-
-        // let stmt = block_on(stmt_future).unwrap();
-        let name_param: &[&(dyn ToSql + Sync)] = &[&name];
-        let result = (*self).client.query_one(&stmt, name_param).await;
+        let result = query::find_one_by_name(&(*self).client, name.clone()).await;
         println!("second block_on for row");
         if result.is_err() {
             return false;
