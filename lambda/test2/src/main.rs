6e7c7e46b83f25a509b1d4da065afcf9d6535fa2
binhle410 <binh@sunrise.vn>
2021-05-26 22:46:40 +0700

    Trigger lambda re-deployment

--- lambda/test2/src/main.rs
+++ lambda/test2/src/main.rs
@@ -15,7 +15,7 @@ async fn test2(req: Request, _: Context) -> Result<impl IntoResponse, Error> {
     let default_header_value = HeaderValue::from_str("anonymous").unwrap();
     let auth_header = req
         .headers()
-        .get("X-Username")
+        .get("x-username")
         .unwrap_or(&default_header_value);
     // creating an application/json response
     Ok(json!({
