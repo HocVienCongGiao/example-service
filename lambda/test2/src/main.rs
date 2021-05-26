a25eaf208fc114a029c5113027b6a647fbcdd686
nhuthm080280 <nhuthm080280@gmail.com>
2021-05-26 20:55:35 +0700

    Test update message

--- lambda/test2/src/main.rs
+++ lambda/test2/src/main.rs
@@ -19,7 +19,7 @@ async fn test2(req: Request, _: Context) -> Result<impl IntoResponse, Error> {
         .unwrap_or(&default_header_value);
     // creating an application/json response
     Ok(json!({
-    "message": "Test 333 is me, how are you? ".to_owned() + auth_header.to_str().unwrap()
+    "message": "Test 222 is me, how are you? ".to_owned() + auth_header.to_str().unwrap()
     }))
 }
 
@@ -31,7 +31,7 @@ mod tests {
     async fn test2_handles() {
         let request = Request::default();
         let expected = json!({
-        "message": "Test 222 is me, how are you? anonymous"
+        "message": "Test 333 is me, how are you? anonymous"
         })
         .into_response();
         let response = test2(request, Context::default())
