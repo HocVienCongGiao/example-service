8d973653b9d5ce7b7f13915cb69e425817f8662e
binhle410 <binh@sunrise.vn>
2021-05-29 00:02:12 +0700

    Remove subnet and sg params

--- lambda/terraform/create-lambda-function-test2.tf
+++ lambda/terraform/create-lambda-function-test2.tf
@@ -9,8 +9,6 @@ module "service-function" {
   s3_key = "dev-sg-hocvienconggiao/example-service/latest/test2.zip"
   function_name = "exampleservice-test2"
   handler = "test2"
-  subnet_ids = data.aws_subnet_ids.private.ids
-  security_group_ids = data.aws_security_groups.sg.ids
   path_part = var.path_part_test2
   http_method = var.http_method
   authorization = var.authorization
