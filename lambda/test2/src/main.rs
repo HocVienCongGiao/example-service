337629dab95858ca1344e8ece05e46e1518784cb
binhle410 <binh@sunrise.vn>
2021-05-30 23:46:37 +0700

    Fix missing import

--- lambda/test2/src/main.rs
+++ lambda/test2/src/main.rs
@@ -2,6 +2,7 @@ use lambda_http::http::header::{
     ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_ORIGIN,
     CONTENT_TYPE,
 };
+use lambda_http::http::HeaderValue;
 use lambda_http::{handler, lambda, Body, Context, IntoResponse, Request, Response};
 use serde_json::json;
 
