eaf0befd3afc7623d6e53524ef345c293a57afdd
binhle410 <binh@sunrise.vn>
2021-05-30 22:33:37 +0700

    Try enabling CORS

--- lambda/test2/src/main.rs
+++ lambda/test2/src/main.rs
@@ -1,5 +1,8 @@
-use lambda_http::http::HeaderValue;
-use lambda_http::{handler, lambda, Context, IntoResponse, Request};
+use lambda_http::http::header::{
+    ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_ORIGIN,
+    CONTENT_TYPE,
+};
+use lambda_http::{handler, lambda, Body, Context, IntoResponse, Request, Response};
 use serde_json::json;
 
 type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
@@ -18,9 +21,26 @@ async fn test2(req: Request, _: Context) -> Result<impl IntoResponse, Error> {
         .get("x-username")
         .unwrap_or(&default_header_value);
     // creating an application/json response
-    Ok(json!({
-    "message": "Test 333 is me, how are you? ".to_owned() + auth_header.to_str().unwrap()
-    }))
+    // Ok(json!({
+    // "message": "Test 2 is me, how are you?"
+    // }))
+    let value = json!(
+        {
+            "message": "Test 2 is me, how are you?"
+        }
+    );
+    let response: Response<Body> = Response::builder()
+        .header(CONTENT_TYPE, "application/json")
+        .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        .header(ACCESS_CONTROL_ALLOW_HEADERS, "*")
+        .header(ACCESS_CONTROL_ALLOW_METHODS, "*")
+        .body(
+            serde_json::to_string(&value)
+                .expect("unable to serialize serde_json::Value")
+                .into(),
+        )
+        .expect("unable to build http::Response");
+    Ok(response)
 }
 
 #[cfg(test)]
