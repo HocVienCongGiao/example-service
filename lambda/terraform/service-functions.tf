f24e50eb52aec21dc323b6b60a38abbf94cd99af
ProjectHocVienCongGiao <84091963+ProjectHocVienCongGiao@users.noreply.github.com>
2021-06-08 20:20:22 +0700

    Update service-functions.tf

--- lambda/terraform/service-functions.tf
+++ lambda/terraform/service-functions.tf
@@ -1,7 +1,7 @@
 module "test2" {
   source = "git::ssh://git@github.com/HocVienCongGiao/terraform-infra.git//skeleton/services/service-function"
   service_name = var.service_name
-  parent_id = module.example-service.api_gateway_resource_id
+  parent_id = module.example-service.query_api_gateway_resource_id
   function_name = "test2"
 
   environment = var.environment
