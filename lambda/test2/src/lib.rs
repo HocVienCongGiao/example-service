6693badd35af712a3585b36f22c7d0590d82b455
binhle410 <binh@sunrise.vn>
2021-06-17 16:31:37 +0700

    could this be it?

--- lambda/test2/src/lib.rs
+++ lambda/test2/src/lib.rs
@@ -19,7 +19,10 @@ pub async fn test2(req: Request, ctx: Context) -> Result<impl IntoResponse, Erro
     // "message": "Test 2 is me, how are you?"
     // }))
     println!("auth_header is {}", auth_header.to_str().unwrap());
-    println!("context.identity.cognitoIdentityId is {:?}", ctx.identity.unwrap().cognitoIdentityId);
+    println!(
+        "context.identity.cognitoIdentityId is {:?}",
+        ctx.identity.unwrap().identity_id
+    );
     let value = json!(
         {
             "message": "Test 2 20210616 is me, how are you?"
