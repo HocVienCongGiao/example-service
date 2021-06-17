c74b18c26d18dd70ba13121d5bd1d57e1adbf61c
binhle410 <binh@sunrise.vn>
2021-06-17 22:55:54 +0700

    All s good now.

--- lambda/test2/src/lib.rs
+++ lambda/test2/src/lib.rs
@@ -16,6 +16,8 @@ struct TokenPayload {
     // from a JSON field called `cognito:username`.
     #[serde(rename(deserialize = "cognito:username"))]
     username: String,
+    #[serde(rename(deserialize = "cognito:groups"))]
+    groups: Vec<String>,
 }
 
 /**
@@ -39,19 +41,22 @@ struct TokenPayload {
 
 pub async fn test2(req: Request, ctx: Context) -> Result<impl IntoResponse, Error> {
     // `serde_json::Values` impl `IntoResponse` by default
-    let default_header_value = HeaderValue::from_str("anonymous12").unwrap();
+    let default_header_value = HeaderValue::from_str("Bearer eyJraWQiOiJaTGpneG41SStaZEpldnJRb0lpMTZEWEZoRHI4eG9UbVZ2b2ZuVm5vb3RFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJmZDlhN2FmOC1mYTc2LTRiODYtYWYzZC1kOTYzNGVmNTIzNzQiLCJhdWQiOiIxcmF2NDExbmNjbnA3M2h0b3BiaG1sOHM2MSIsImNvZ25pdG86Z3JvdXBzIjpbIk9wZXJhdG9yR3JvdXAiXSwiZXZlbnRfaWQiOiI5NjQ1ZDYyMi0zZjRiLTQyYjctOWI0ZC03MWQzNWRhOTI1NmQiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTYyMzkzNDkyNiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLXNvdXRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLXNvdXRoZWFzdC0xXzlRV1NZR3pYayIsInBob25lX251bWJlcl92ZXJpZmllZCI6dHJ1ZSwiY29nbml0bzp1c2VybmFtZSI6ImRldi1vcGVyYXRvciIsInBob25lX251bWJlciI6Iis4NDM2OTE0MDkxNiIsImV4cCI6MTYyMzk0ODAwOCwiaWF0IjoxNjIzOTQ0NDA4fQ.ml3N8J7uw4rbQOneEdnmQW6OwsAY6ycmp5PIrKGZKF3yWQn0oQECIhF2Q_jjWOjWPikpUQEy5IKgghiJLukgKo7q-T4tUauPG3GJxoSGQkfVcglkNu8nZTu7ioxXzlQAWsXLakgkH40mGzI6kl2hkEhRQh_lWGrT7TqDP2yVTsDMKEGJBdtcb-kFCnYHfn9FMoCyVGo4K3tSrkeGno7bzwO_XpFtZRhv9Qs4OtfESXARYCP3St69hyf4JuAop6-Zb38FPWcp6rnpRG3BF64YPGqo0J0MAyWVz_Du7Pk3-H5uZqqrr6iHKoPwoabPPlZxJ3JGdifVt_I54SwTbelbzw").unwrap();
     let auth_header_value = req
         .headers()
         .get("authorization")
         .unwrap_or(&default_header_value);
     let auth_header_str = auth_header_value.to_str().unwrap();
     let username: String;
+    let groups: Vec<String>;
     if (auth_header_str != "anonymous12") {
         let jwt_token = &auth_header_str.to_string()[7..];
         let token_data: TokenData<TokenPayload> =
             jsonwebtoken::dangerous_insecure_decode(jwt_token).unwrap();
         let token_payload = token_data.claims;
         username = token_payload.username;
+        groups = token_payload.groups;
+        println!("Groups include {:?}", groups);
     } else {
         username = String::from("anonymous");
     }
