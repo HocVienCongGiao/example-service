c663e187313cad4c1bde7dd0aa5f1f4e9c4e14ff
binhle410 <binh@sunrise.vn>
2021-06-21 22:21:32 +0700

    ok

--- example-dynamodb-lambda/my-table/src/main.rs
+++ example-dynamodb-lambda/my-table/src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<(), Error> {
 }
 
 async fn func(event: Value, _: Context) -> Result<Value, Error> {
-    let first_name = event["firstName"].as_str().unwrap_or("world");
+    // let first_name = event["firstName"].as_str().unwrap_or("world");
 
-    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
+    Ok(json!({ "message": format!("Hello, {:?}!", event) }))
 }
