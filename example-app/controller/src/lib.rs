c3751bb8806887ff71a946bc7f06eab9146d306c
HieuMinh67 <minhhieu060799@gmail.com>
2021-06-21 08:41:45 +0700

    find pets by name

--- example-app/controller/src/lib.rs
+++ example-app/controller/src/lib.rs
@@ -22,6 +22,24 @@ async fn get_test1() -> Test1SimpleQueryResponse {
         .await
 }
 
+pub async fn find_pets_by_name() -> openapi::test1::Pet {
+    let response = get_test2().await;
+    response.to_openapi()
+}
+
+async fn get_test2() -> Test1SimpleQueryResponse {
+    let client = db_postgres::connect().await;
+    let client = db_postgres::migrate(client).await;
+
+    let test1_repository = Test1SimpleRepository { client };
+
+    domain::interactors::test1_query::Test1SimpleQueryInteractor::new(test1_repository)
+        .get_test1(Test1SimpleQueryRequest {
+            name: "Ngo Dinh Nhu".to_string(),
+        })
+        .await
+}
+
 #[cfg(test)]
 mod tests {
 
