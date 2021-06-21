6b7c458473be6746c8d1681bc29ce6c02c20550b
binhle410 <binh@sunrise.vn>
2021-06-21 22:07:01 +0700

    event_source_arn = "arn:aws:dynamodb:ap-southeast-1:891616054205:table/dev-sg_MyTable/stream/2021-06-21T05:18:41.574"

--- example-dynamodb-lambda/terraform/dynamodb-functions.tf
+++ example-dynamodb-lambda/terraform/dynamodb-functions.tf
@@ -3,7 +3,8 @@ module "test2" {
   service_name = var.service_name
 
   function_name = "my-table"
-    
+  event_source_arn = "arn:aws:dynamodb:ap-southeast-1:891616054205:table/dev-sg_MyTable/stream/2021-06-21T05:18:41.574"
+
   environment = var.environment
   db_host              = var.db_host
   db_user              = var.db_user
