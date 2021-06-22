179b0e5a2302e0f4f9f6f325a8ae38ff663acca9
HieuMinh67 <minhhieu060799@gmail.com>
2021-06-22 10:44:46 +0700

    Update service-functions.tf

--- lambda/terraform/service-functions.tf
+++ lambda/terraform/service-functions.tf
@@ -15,3 +15,21 @@ module "test2" {
   db_password          = var.db_password
   db_name              = var.db_name
 }
+
+module "test3" {
+  source = "git::ssh://git@github.com/HocVienCongGiao/terraform-infra.git//skeleton/services/service-function"
+  service_name = var.service_name
+  query_api_parent_id = module.example-service.query_api_gateway_resource_id
+  mutation_api_parent_id = module.example-service.mutation_api_gateway_resource_id
+
+  function_name = "test3"
+  depends_on = [
+    module.example-service
+   ]
+
+  environment = var.environment
+  db_host              = var.db_host
+  db_user              = var.db_user
+  db_password          = var.db_password
+  db_name              = var.db_name
+}
