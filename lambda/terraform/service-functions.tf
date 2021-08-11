0a6954e6a9ab1fbbbb0df79b8268cb415a8a29a6
Peter Bean <76221624+peterbean410@users.noreply.github.com>
2021-08-11 20:40:48 +0700

    Update service-functions.tf

--- lambda/terraform/service-functions.tf
+++ lambda/terraform/service-functions.tf
@@ -36,6 +36,26 @@ module "test2_id" {
   db_name              = var.db_name
 }
 
+module "test2_id_sub" {
+  source = "git::ssh://git@github.com/HocVienCongGiao/terraform-infra.git//skeleton/services/service-function"
+  service_name = var.service_name
+  query_api_parent_id = module.test2_id.query_api_gateway_resource_id
+  mutation_api_parent_id = module.test2_id.mutation_api_gateway_resource_id
+    
+  function_name = "test2_id"
+  file_name     = "test2"
+  path_part     = "sub"
+  depends_on = [
+    module.example-service
+   ]
+    
+  environment = var.environment
+  db_host              = var.db_host
+  db_user              = var.db_user
+  db_password          = var.db_password
+  db_name              = var.db_name
+}
+
 module "test3" {
   source = "git::ssh://git@github.com/HocVienCongGiao/terraform-infra.git//skeleton/services/service-function"
   service_name = var.service_name
